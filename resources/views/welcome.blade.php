ffbdecba6bbcca2bfe2666b5a5693869e68e89af
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-09-25 12:54:28 -0300

    feat: add global CSS file and update styles in add, initial, and welcome views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
     <title>Welcome</title>
+
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
 </head>
 
 <body>
